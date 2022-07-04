fd0cb989ecc5cf1064a3e39100cee1fb2f4c645b
Manish754 <79052143+Manish754@users.noreply.github.com>
2022-07-04 12:46:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    return 'Hello, Docker!'
+def home():
+    return {'msg': 'working testing dbbbb'}
+
+
+if _name_ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
